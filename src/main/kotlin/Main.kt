ec2bc43bd2dd6c8b09398882bc95af6c911c9a10
AndreyOvch <amo-rzn@ya.ru>
2020-10-16 22:05:19 +0300

    Rename fun

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
 fun main() {
-    println("Коммисия за перевод: ${main2("Maestro",10000000,amount = 1000000)} коп.")
+    println("Коммисия за перевод: ${typeCommission("Maestro",10000000,amount = 1000000)} коп.")
 }
 
-fun main2 (cardType:String = "VKPay", prevAmount:Int=0, amount:Int):Double {
+fun typeCommission (cardType:String = "VKPay", prevAmount:Int=0, amount:Int):Double {
     val commission = if(prevAmount >= 75000*100) amount*0.6/100+2000 else 0.0
     val resultCommission = when(cardType){
         "MasterCard" -> commission

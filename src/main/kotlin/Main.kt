5f2e22769a5a684a34db79628fc00c6d91540aec
AndreyOvch <amo-rzn@ya.ru>
2020-10-19 23:08:20 +0300

    test 80 %

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,12 @@
 fun main() {
-    println("Коммисия за перевод: ${typeCommission("Maestro", 74000*100, 1000000)} коп.")
+    println("Коммисия за перевод: ${typeCommission("Maestro", 76000*100, 10000*100)} коп.")
 }
 
-fun typeCommission(cardType: String = "VKPay", prevAmount: Int = 0, amount: Int): Double {
-    val commission = if (prevAmount >= 75000 * 100) amount * 0.6 / 100 + 2000 else 0.0
+fun typeCommission(cardType: String, prevAmount: Int, amount: Int): Double {
+    //val commission = if (prevAmount >= 75000 * 100) amount * 0.6 / 100 + 2000 else 0.0
     val resultCommission = when (cardType) {
-        "MasterCard" -> commission
-        "Maestro" -> commission
+        "MasterCard" -> if (prevAmount >= 75000 * 100) amount * 0.6 / 100 + 2000 else 0.0
+        "Maestro" -> if (prevAmount >= 75000 * 100) amount * 0.6 / 100 + 2000 else 0.0
         else -> 0.0
     }
     return resultCommission
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -5,30 +5,89 @@ import org.junit.Assert.*
 class MainKtTest {
 
     @Test
-    fun typeCommission_yes() {
+    fun typeCommission_yes_Maestro() {
         val cardType = "Maestro"
-        val prevAmount = 10000000
-        val amount = 1000000
+        val prevAmount = 76000 * 100
+        val amount = 10000 * 100
 
-        val result:Double = typeCommission(
+        val result: Double = typeCommission(
             cardType = cardType,
             prevAmount = prevAmount,
             amount = amount
         )
-        assertEquals(8000.0, result,0.0)
+
+        assertEquals(8000.0, result, 0.0)
     }
 
     @Test
-    fun typeCommission_not() {
+    fun typeCommission_not_Maestro() {
         val cardType = "Maestro"
-        val prevAmount = 74000*100
-        val amount = 1000000
+        val prevAmount = 74000 * 100
+        val amount = 10000 * 100
 
-        val result:Double = typeCommission(
+        val result: Double = typeCommission(
             cardType = cardType,
             prevAmount = prevAmount,
             amount = amount
         )
-        assertEquals(0.0, result,0.0)
+        assertEquals(0.0, result, 0.0)
     }
+
+    @Test
+    fun typeCommission_yes_VKPay() {
+        val cardType = "VKPay"
+        val prevAmount = 760000 * 100
+        val amount = 10000 * 100
+
+        val result: Double = typeCommission(
+            cardType = cardType,
+            prevAmount = prevAmount,
+            amount = amount
+        )
+        assertEquals(0.0, result, 0.0)
+    }
+
+    @Test
+    fun typeCommission_not_VKPay() {
+        val cardType = "VKPay"
+        val prevAmount = 0
+        val amount = 1000000
+
+        val result: Double = typeCommission(
+            cardType = cardType,
+            prevAmount = prevAmount,
+            amount = amount
+        )
+        assertEquals(0.0, result, 0.0)
+    }
+
+    @Test
+    fun typeCommission_yes_MasterCard() {
+        val cardType = "MasterCard"
+        val prevAmount = 10000000
+        val amount = 1000000
+
+        val result: Double = typeCommission(
+            cardType = cardType,
+            prevAmount = prevAmount,
+            amount = amount
+        )
+        assertEquals(8000.0, result, 0.0)
+    }
+
+    @Test
+    fun typeCommission_not_MasterCard() {
+        val cardType = "MasterCard"
+        val prevAmount = 0
+        val amount = 1000000
+
+        val result: Double = typeCommission(
+            cardType = cardType,
+            prevAmount = prevAmount,
+            amount = amount
+        )
+        assertEquals(0.0, result, 0.0)
+
+    }
+
 }
